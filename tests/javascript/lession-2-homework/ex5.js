47ac88447a7cfbfda20f207a69736ba9e28504eb
Trang <saranguyen0001@gmail.com>
2025-06-16 22:29:49 +0700

    fix:fix homework

--- tests/javascript/lession-2-homework/ex5.js
+++ tests/javascript/lession-2-homework/ex5.js
@@ -1,8 +1,8 @@
 for (let i = 1; i <= 100; i++) {
-    if (i%2 === 0) {
-        console.log('số <i> là số chẵn');
+    if (i % 2 === 0) {
+        console.log(`số ${i} là số chẵn`);
     }
-    if (i%2 === 1){
-        console.log('số <i> là số lẻ');
+    if (i % 2 === 1) {
+        console.log(`số ${i} là số lẻ`);
     }
 }
